aa9c10abc89569cf82fcb6ed622983d30f124e8d
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-09-03 22:51:59 +0530

    vite react

--- o1vitereact/src/App.tsx
+++ o1vitereact/src/App.tsx
@@ -1,10 +1,11 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = "chai aur code"
   return (
     <>
     <Chai />
-    <h1>Mandeep Mukhda</h1>
+    <h1>Mandeep Mukhda {username}</h1>
     <p>Test Para</p>
     </>
   )
--- o1vitereact/src/main.tsx
+++ o1vitereact/src/main.tsx
@@ -1,7 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+function MyApp(){
+    return(
+        <div>
+            <h1>MANDEEP MUKHDA</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'
+// }
+
+const reactElement =  React.createElement(
+    'a',
+    {
+        href: 'https://google.com',target: '_blank'
+    },
+    'click me to visit google'
+)
+
+const anotherElement= (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <MyApp/>
+    // reactElement
+    // anotherElement
     <App/>
 )
